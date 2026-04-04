df02674919267c96b81c2584bf58665bfb7509be
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-04-04 11:26:06 +0300

    Update Cone.cpp

--- Cone.cpp
+++ Cone.cpp
@@ -5,7 +5,9 @@
 #include <stdexcept>
 
 using namespace std;
-
+/**
+ * @brief Реализация конструктора с проверкой корректности параметров.
+ */
 Cone::Cone(double cx, double cy, double cz, double r, double h)
     : x(cx), y(cy), z(cz), radius(r), height(h)
 {
@@ -19,16 +21,22 @@ void Cone::validate() const {
     if (height <= 0)
         throw invalid_argument("Height must be greater than 0.");
 }
-
+/**
+ * @brief Вычисляет образующую конуса.
+ */
 double Cone::slantHeight() const {
     return sqrt(radius * radius + height * height);
 }
-
+/**
+ * @brief Вычисляет полную площадь поверхности конуса.
+ */
 double Cone::surfaceArea() const {
     double l = slantHeight();
     return M_PI * radius * (radius + l);
 }
-
+/**
+ * @brief Печатает параметры конуса и площадь поверхности.
+ */
 void Cone::print() const {
     cout << "Center:: (" << x << ", " << y << ", " << z << ")" << endl;
     cout << "Radius: " << radius << endl;
