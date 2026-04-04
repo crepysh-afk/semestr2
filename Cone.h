428547630768a6ea1f85a7a0793a8421cd446300
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-04-04 11:24:13 +0300

    Update Cone.h

--- Cone.h
+++ Cone.h
@@ -1,6 +1,13 @@
 #ifndef CONE_H
 #define CONE_H
 
+/**
+ * @class Cone
+ * @brief Класс, описывающий геометрический конус.
+ *
+ * Конус задаётся координатами центра основания,
+ * радиусом основания и высотой.
+ */
 class Cone {
 private:
     double x, y, z;
@@ -10,11 +17,28 @@ private:
     void validate() const;   
 
 public:
+/**
+     * @brief Конструктор класса Cone.
+     * @param cx Координата X центра основания
+     * @param cy Координата Y центра основания
+     * @param cz Координата Z центра основания
+     * @param r Радиус основания (должен быть > 0)
+     * @param h Высота конуса (должна быть > 0)
+     */
     Cone(double cx, double cy, double cz, double r, double h);
-
+/**
+     * @brief Вычисляет длину образующей конуса.
+     * @return Длина образующей
+     */
     double slantHeight() const;
+ /**
+     * @brief Вычисляет полную площадь поверхности конуса.
+     * @return Площадь поверхности
+ */
     double surfaceArea() const;
-
+/**
+     * @brief Выводит информацию о конусе в консоль.
+*/
     void print() const;
 };
 
