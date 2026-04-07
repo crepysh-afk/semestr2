f6cb6025518d100f2982f7de7a82038283dc2b76
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-04-07 15:26:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ int main() {
     setlocale(LC_ALL, "RU");
 
     try {
-        Cone cone(0, 0, 0, 5, 12);
-        cone.print();
+        double x,y,z;
+        double radius, height;
 
+        cin>>x >>y >>z;
+        cin >> radius;
+        cin >> height;
+
+        Cone cone(x,y,z, radius, height);
+
+        cout<<"\n Cone created successfully\n";
+        cone.print();
     }
     catch (const exception& e) {
         cout << "Error: " << e.what() << endl;
