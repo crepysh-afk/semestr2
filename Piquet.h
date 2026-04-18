5454e546ec6635a484d29434ba57b3070967993d
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2026-04-18 13:35:17 +0300

    Add files via upload

---
+++ Piquet.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+
+/**
+ * @brief Класс Пикет.
+ */
+class Piquet
+{
+private:
+    int number;          ///< Номер пикета
+    double shift;        ///< Смещение
+    bool isStandard;     ///< Стандартный ли пикет
+
+public:
+    Piquet(int number, double shift, bool isStandard = true);
+
+    std::string ToString() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Piquet& p);
+    friend std::istream& operator>>(std::istream& is, Piquet& p);
+
+    bool operator<(const Piquet& other) const;
+    bool operator==(const Piquet& other) const;
+};
